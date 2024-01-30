7cc7ab91fe89b0e0e8c88021e9f47f32211d8da7
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-01-30 18:21:47 -0500

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -9,6 +9,8 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "No"
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
@@ -30,6 +32,6 @@ if __name__ == "__main__":
 #"Is it possible to decode a message without a key? - Yes/No": "No"
 #"Is a hashed message supposed to be un-hashed? - Yes/No": "Yes"
 #"What is the SHA256 hashing value of your NYU email and use the answer in your code - ": 
-#"Is MD5 a secured hashing algorithm? - No"
+#"Is MD5 a secured hashing algorithm? - Yes/No": 
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number: 7" 
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 3
