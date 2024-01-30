47b497a31cae7d59681ec0e7dcfb9fdb6d6d0d67
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-01-30 18:06:42 -0500

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -30,6 +30,6 @@ if __name__ == "__main__":
 #"Is it possible to decode a message without a key? - Yes/No": No
 #"Is a hashed message supposed to be un-hashed? - Yes/No": Yes
 #"What is the SHA256 hashing value of your NYU email and use the answer in your code - ": 
-#"Is MD5 a secured hashing algorithm? - Yes/No": No
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 7
+#"Is MD5 a secured hashing algorithm? - No"
+#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number: 7" 
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 3
