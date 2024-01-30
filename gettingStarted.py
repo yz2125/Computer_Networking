6f5936ab0de5496cebaca6b7751151d3846efaf4
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-01-30 18:15:52 -0500

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -24,11 +24,11 @@ if __name__ == "__main__":
     print(welcome_assignment_answers(debug_question))
 
 #Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": pcap
-#"Are encoding and encryption the same? - Yes/No": No
-#"Is it possible to decrypt a message without a key? - Yes/No": No
-#"Is it possible to decode a message without a key? - Yes/No": No
-#"Is a hashed message supposed to be un-hashed? - Yes/No": Yes
+#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": "pcap"
+#"Are encoding and encryption the same? - Yes/No": "No"
+#"Is it possible to decrypt a message without a key? - Yes/No": "No"
+#"Is it possible to decode a message without a key? - Yes/No": "No"
+#"Is a hashed message supposed to be un-hashed? - Yes/No": "Yes"
 #"What is the SHA256 hashing value of your NYU email and use the answer in your code - ": 
 #"Is MD5 a secured hashing algorithm? - No"
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number: 7" 
