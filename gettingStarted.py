08ccb29b14258980061b53c1f7ae4bcb7ae725f8
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-01-30 18:47:23 -0500

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -8,7 +8,7 @@ def welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer == 'pcap'
+        answer = 'pcap'
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
