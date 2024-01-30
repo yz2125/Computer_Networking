4671b581c9f6d34fb83b53be55e23311ff78a7a6
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-01-30 18:35:34 -0500

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -18,7 +18,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "7"
+        answer = int(7)
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer == "pcap"
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
