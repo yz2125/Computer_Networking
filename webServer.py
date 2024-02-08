99838a7ccd2dfc441431f1b3a69f4cb139305f69
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-02-07 19:57:41 -0500

    webServer.py

--- webServer.py
+++ webServer.py
@@ -2,8 +2,8 @@
 from socket import *
 # In order to terminate the program
 import sys
-
-
+HOST = '127.0.0.1'
+PORT = 6789
 
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
